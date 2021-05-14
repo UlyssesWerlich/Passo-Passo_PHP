b1f5aa495a5b384e7214e0e93dfd4e22014a18aa
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-05-14 12:45:04 -0300

    005 - Inserindo arquivos CSS no projeto.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <head>
         <meta charset='utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='/styles/style.css'>
         <title>Passo a passo</title>
     </head>
 
