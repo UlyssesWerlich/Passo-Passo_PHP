bfda84398181861188e1a69b295ca31864e8724f
Ulysses Werlich <werlichborgesulysses@gmail.com>
2021-05-14 12:24:12 -0300

    003 - Gerando a estrutura HTML com o comando echo. Criando a primeira variável no PHP.

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    echo "Olá mundo";
+
+    $nome_usuario = "Ulysses Werlich Borges";
+
+    echo "
+    <!DOCTYPE html>
+    <html lang='pt-br'>
+
+        <head>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'>
+            <title>Passo a passo</title>
+        </head>
+
+        <body>
+            <h1>Passo a Passo</h1>
+            <h3>Seja bem vindo, $nome_usuario.</h3>
+        </body>
+
+    </html>
+    ";
 ?>
